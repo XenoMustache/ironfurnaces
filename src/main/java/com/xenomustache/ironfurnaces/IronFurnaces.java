ce5203be3775a42646b425da3bba835d4c6cc9cd
XenoMustache <xenomustache@gmail.com>
2018-08-23 19:29:03 -0700

    Edited info file, minor tweaks

--- src/main/java/com/xenomustache/ironfurnaces/IronFurnaces.java
+++ src/main/java/com/xenomustache/ironfurnaces/IronFurnaces.java
@@ -17,19 +17,16 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
-@Mod(modid = IronFurnaces.MODID, name = IronFurnaces.NAME, version = IronFurnaces.VERSION)
+@Mod(modid = IronFurnaces.MODID, name = IronFurnaces.NAME, version = IronFurnaces.VERSION, acceptedMinecraftVersions = "1.12.2")
 public class IronFurnaces {
     public static final String MODID = "ironfurnaces";
     public static final String NAME = "Iron Furnaces";
     public static final String VERSION = "1.3";
-
+    public static final ModTab creativeTab = new ModTab();
     @SidedProxy(serverSide = "com.xenomustache.ironfurnaces.proxy.CommonProxy", clientSide = "com.xenomustache.ironfurnaces.proxy.ClientProxy")
     public static CommonProxy proxy;
-
     @Mod.Instance(MODID)
     public static IronFurnaces instance;
-
-    public static final ModTab creativeTab = new ModTab();
 
     @Mod.EventHandler
     public void preinit(FMLPreInitializationEvent event) {
@@ -70,7 +67,7 @@ public class IronFurnaces {
         }
     }
 
-    public static class ModTab extends CreativeTabs{
+    public static class ModTab extends CreativeTabs {
         public ModTab() {
             super(IronFurnaces.MODID);
         }
